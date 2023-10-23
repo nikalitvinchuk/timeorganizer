ba353a92cbfd6008a678f6c420a5692c9c191639
Kuba Buczek <kuba1buczek@gmail.com>
2023-10-23 13:31:20 +0200

    SqlModels

---
+++ timeorganizer/DatabaseModels/LoginModel.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timeorganizer.DatabaseModels
+{
+    public class LoginModel
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool isAdmin { get; set; }
+        public bool RememberMe { get; set; } = false;
+        public DateTime DataCreated { get; set; }
+
+    }
+}
---
+++ timeorganizer/DatabaseModels/TaskComponentsModel.cs
@@ -0,0 +1,29 @@
+﻿using GoogleGson;
+using Java.Lang;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace timeorganizer.DatabaseModels
+{
+    internal class TaskComponentsModel
+    {
+        [PrimaryKey]
+        public int Id { get; set; } // id zadania
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int TaskId { get; set; }
+        public int UserId { get; set; }
+        public string Status { get; set; }
+        public bool TaskComplited { get; set; }
+        public bool IsActive { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastUpdated { get; set; }
+
+
+    }
+}
