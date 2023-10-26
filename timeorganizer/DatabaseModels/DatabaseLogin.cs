bc937d45d847a922a0b8ec31f366b8348d0d11db
Kuba Buczek <kuba1buczek@gmail.com>
2023-10-26 09:38:51 +0200

    poprawki

---
+++ timeorganizer/DatabaseModels/DatabaseLogin.cs
@@ -0,0 +1,81 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timeorganizer.DatabaseModels 
+{
+    public class DatabaseLogin : IAsyncDisposable // przykładowy plik jak korzystać z połączenia do bazy danych stworzony dla rejestracji RegisterPageviewModel - dane podane z palca
+    {
+        private const string DbName = "Timeorgranizer.db3";
+        private static string DbPath => Path.Combine(FileSystem.AppDataDirectory, DbName);
+
+        private SQLiteAsyncConnection _connection;
+        private SQLiteAsyncConnection Database =>
+            (_connection ??= new SQLiteAsyncConnection(DbPath)); // polączenie do bazy
+
+        private async Task CreateTableIfNotExists<TTable>() where TTable : class, new() //tworzenie tabel z klas
+        {
+            await Database.CreateTableAsync<TTable>();
+        }
+
+        private async Task<AsyncTableQuery<TTable>> GetTableAsync<TTable>() where TTable : class, new()  //odczytanie tabeli
+        {
+            await CreateTableIfNotExists<TTable>();
+            return Database.Table<TTable>();
+        }
+
+        public async Task<IEnumerable<TTable>> GetAllAsync<TTable>() where TTable : class, new() //odczyt z tabeli 
+        {
+            var table = await GetTableAsync<TTable>();
+            return await table.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TTable>> GetFileteredAsync<TTable>(Expression<Func<TTable, bool>> predicate) where TTable : class, new() //wyszukiwanie w tabeli - przekazujemy funckje
+        {
+            var table = await GetTableAsync<TTable>();
+            return await table.Where(predicate).ToListAsync();
+        }
+
+        private async Task<TResult> Execute<TTable, TResult>(Func<Task<TResult>> action) where TTable : class, new() 
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await action();
+        }
+
+        public async Task<TTable> GetItemByKeyAsync<TTable>(object primaryKey) where TTable : class, new() // odczyt po kluczu glowym z tabeli np ID. 
+        {
+            return await Execute<TTable, TTable>(async () => await Database.GetAsync<TTable>(primaryKey));
+        }
+
+        public async Task<bool> AddItemAsync<TTable>(TTable item) where TTable : class, new() //dodawanie do tabeli
+        {
+
+            return await Execute<TTable, bool>(async () => await Database.InsertAsync(item) > 0);
+        }
+
+        public async Task<bool> UpdateItemAsync<TTable>(TTable item) where TTable : class, new() //aktualziacja w tabeli np zmiana statusu. Przekazujemy caly wiersz zgodny z struktura np Users
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await Database.UpdateAsync(item) > 0;
+        }
+
+        public async Task<bool> DeleteItemAsync<TTable>(TTable item) where TTable : class, new()  //usuwanie po obiekcie
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await Database.DeleteAsync(item) > 0;
+        }
+
+        public async Task<bool> DeleteItemByKeyAsync<TTable>(object primaryKey) where TTable : class, new() //usuwanie z tabeli
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await Database.DeleteAsync<TTable>(primaryKey) > 0;
+        }
+
+        public async ValueTask DisposeAsync() => await _connection?.CloseAsync(); // zamkniecie polaczenia z baza
+    }
+}
+
